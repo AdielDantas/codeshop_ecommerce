0c89be4feb2cbb16866bf8c21c4bd29ea13495be
Adiel Dantas <adieldantasn@gmail.com>
2024-11-27 15:24:32 -0300

    Tratamento de exceções

---
+++ src/main/java/com/codeshop/ecommerce/services/exception/DataBaseException.java
@@ -0,0 +1,8 @@
+package com.codeshop.ecommerce.services.exception;
+
+public class DataBaseException extends RuntimeException {
+
+    public DataBaseException(String msg) {
+        super(msg);
+    }
+}
---
+++ src/main/java/com/codeshop/ecommerce/services/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.codeshop.ecommerce.services.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String msg) {
+        super(msg);
+    }
+}
