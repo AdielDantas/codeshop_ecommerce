e9b790302068defa9d61df15e0e8c7dd2d249da8
Adiel Dantas <adieldantasn@gmail.com>
2025-02-13 21:03:59 -0300

    UserDetailsService

---
+++ src/main/java/com/codeshop/ecommerce/services/UserService.java
@@ -0,0 +1,36 @@
+package com.codeshop.ecommerce.services;
+
+import com.codeshop.ecommerce.entities.Role;
+import com.codeshop.ecommerce.entities.User;
+import com.codeshop.ecommerce.projections.UserDetailsProjection;
+import com.codeshop.ecommerce.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.List;
+
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        List<UserDetailsProjection> result = repository.searchUserAndRolesByEmail(username);
+        if (result.size() == 0) {
+            throw new UsernameNotFoundException("Email not found");
+        }
+
+        User user = new User();
+        user.setEmail(result.get(0).getUsername());
+        user.setPassword(result.get(0).getPassword());
+        for (UserDetailsProjection projection : result) {
+            user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
+        }
+
+        return user;
+    }
+}
