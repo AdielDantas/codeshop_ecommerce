c042751f6b0d99e2067aed584f53d502581f6fe5
Adiel Dantas <adieldantasn@gmail.com>
2025-03-15 01:18:33 -0300

    GET categories

---
+++ src/main/java/com/codeshop/ecommerce/controllers/CategoryControllers.java
@@ -0,0 +1,26 @@
+package com.codeshop.ecommerce.controllers;
+
+import com.codeshop.ecommerce.dto.CategoryDTO;
+import com.codeshop.ecommerce.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryControllers {
+
+    @Autowired
+    private CategoryService service;
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> findAll() {
+        List<CategoryDTO> list = service.findAll();
+        return ResponseEntity.ok(list);
+    }
+
+}
