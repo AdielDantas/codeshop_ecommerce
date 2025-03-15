1325b2dbd62452e46ce7e69fbc106dde9160d155
Adiel Dantas <adieldantasn@gmail.com>
2025-03-15 01:27:37 -0300

    Fix validations

--- src/main/java/com/codeshop/ecommerce/dto/ProductDTO.java
+++ src/main/java/com/codeshop/ecommerce/dto/ProductDTO.java
@@ -2,10 +2,7 @@ package com.codeshop.ecommerce.dto;
 
 import com.codeshop.ecommerce.entities.Category;
 import com.codeshop.ecommerce.entities.Product;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +19,7 @@ public class ProductDTO {
     @Size(min = 10, message = "Nome precisa ter no mínimo 10 caracteres")
     private String description;
 
+    @NotNull(message = "Campo requerido")
     @Positive(message = "O preço deve ser positivo")
     private Double price;
     private String imgUrl;
