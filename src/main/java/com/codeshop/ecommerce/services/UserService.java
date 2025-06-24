4154d6207b114e180a582fb8a21e43971d03b4a8
Adiel Dantas <adieldantasn@gmail.com>
2025-06-24 14:25:01 -0300

    Update método authenticated

--- src/main/java/com/codeshop/ecommerce/services/UserService.java
+++ src/main/java/com/codeshop/ecommerce/services/UserService.java
@@ -5,13 +5,11 @@ import com.codeshop.ecommerce.entities.Role;
 import com.codeshop.ecommerce.entities.User;
 import com.codeshop.ecommerce.projections.UserDetailsProjection;
 import com.codeshop.ecommerce.repositories.UserRepository;
+import com.codeshop.ecommerce.util.CustomUserUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,6 +20,9 @@ public class UserService implements UserDetailsService {
 
     @Autowired
     private UserRepository repository;
+
+    @Autowired
+    private CustomUserUtil customUserUtil;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -43,9 +44,7 @@ public class UserService implements UserDetailsService {
 
     protected User authenticated() {
         try {
-            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-            Jwt jwtPrincipal = (Jwt) authentication.getPrincipal();
-            String username = jwtPrincipal.getClaim("username");
+            String username = customUserUtil.getLoggedUserName();
             return repository.findByEmail(username).get();
         }
         catch (Exception e) {
